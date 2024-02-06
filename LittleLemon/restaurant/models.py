a1e68a7eb1930f4b6d56f31e6ec750e9343c0376
jddev273 <59052623+jddev273@users.noreply.github.com>
2024-02-06 01:37:37 -0800

    Added tests

--- LittleLemon/restaurant/models.py
+++ LittleLemon/restaurant/models.py
@@ -6,7 +6,9 @@ class Booking(models.Model):
     BookingDate = models.DateField()
 
 class Menu(models.Model):
-    Title = models.CharField(max_length = 255)
-    Price = models.DecimalField(max_digits = 10, decimal_places = 2)
-    Inventory = models.IntegerField()
+    title = models.CharField(max_length = 255)
+    price = models.DecimalField(max_digits = 10, decimal_places = 2)
+    inventory = models.IntegerField()
 
+    def __str__(self):
+        return f'{self.title} : {str(self.price)}'
